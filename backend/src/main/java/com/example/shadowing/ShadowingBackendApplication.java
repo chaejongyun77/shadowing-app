d88a1df1f24b5e9daf702dca2f1466773f067faf
chaejongyun77 <140371161+chaejongyun77@users.noreply.github.com>
2026-06-23 16:45:39 +0900

    Initial commit: Monorepo setup with Spring Boot & React

---
+++ backend/src/main/java/com/example/shadowing/ShadowingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.shadowing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadowingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShadowingBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/shadowing/ShadowingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shadowing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShadowingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
